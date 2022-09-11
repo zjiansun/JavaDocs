2464129ffd8cdf79cdfa8ce4efe6246038d1cba3
Jian Sun <zjsun.ca@gmail.com>
2022-09-11 19:38:53 -0400

    change package name

---
+++ src/learning/firststeps/challenges/Anagram.java
@@ -0,0 +1,21 @@
+package learning.firststeps.challenges;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String first = sc.nextLine();
+        first = first.toLowerCase();
+        char[] firstWord = first.toCharArray();
+        String second = sc.nextLine();
+        second = second.toLowerCase();
+        char[] secondWord = second.toCharArray();
+
+        Arrays.sort(firstWord);
+        Arrays.sort(secondWord);
+        System.out.println(Arrays.equals(firstWord, secondWord));
+    }
+}
---
+++ src/learning/firststeps/challenges/Anagram.md
@@ -0,0 +1,9 @@
+# Anagrams
+
+Two words are anagrams if they contain the same letters but in a different order. Here are a few examples of anagram pairs:
+
+- “listen” and “silent”
+- “binary” and “brainy”
+- “Paris” and “pairs”
+
+For a given input of two strings, return a Boolean TRUE if the two strings are anagrams.
