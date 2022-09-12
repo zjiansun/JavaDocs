a56920ad1d8f560ac44ecd85819b93921a0a6f57
Jian Sun <zjsun.ca@gmail.com>
2022-09-11 20:03:04 -0400

    prime number checker final

--- src/learning/firststeps/challenges/PrimeCheck.java
+++ src/learning/firststeps/challenges/PrimeCheck.java
@@ -1,4 +1,17 @@
 package learning.firststeps.challenges;
+import java.util.Scanner;
 
 public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int factors = 0;
+
+        for(int i=1;i<=number;i++){
+            if(number%i==0){
+                factors++;
+            }
+        }
+        System.out.println(factors<=2);
+    }
 }
