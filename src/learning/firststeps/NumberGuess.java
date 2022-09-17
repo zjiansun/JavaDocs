6e4c894c9be7296ceb672cb7a27dd082084b2ba2
Jian Sun <zjsun.ca@gmail.com>
2022-09-17 16:28:07 -0400

    soccer match project (adding todos)

--- src/learning/firststeps/NumberGuess.java
+++ src/learning/firststeps/NumberGuess.java
@@ -5,7 +5,7 @@ public class NumberGuess {
     public static void main(String[] args) {
         int randomNum = (int)Math.round(Math.random()*100 + 1);
         Scanner sc = new Scanner(System.in);
-        System.out.println("=========================");
+        System.out.println("==========================");
         System.out.println("Welcome to the game! Guess the number between 1 - 100");
         System.out.println();
         while(true){
